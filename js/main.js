f1456a414ce7ab3400694a01eb8f3ecc577defd8
Chris83 <83odie@gmail.com>
2022-04-07 20:52:25 -0400

    Add clear button functionality

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ function NumberClick(e) {
    const screen = document.querySelector(".screen");
    screen.innerText = screen.innerText + e.innerText;
 }
+
+function ClearClick(){
+   operator = '';
+   term = 0;
+   const screen = document.querySelector(".screen");
+   screen.innerText = '';
+}
